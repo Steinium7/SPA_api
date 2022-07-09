0056de39fe6a169978ee5c45803196d8c0b0548e
Steinium7 <51533598+Steinium7@users.noreply.github.com>
2022-07-09 09:40:07 +0000

    logic addittion and pug

--- routes/employee.ts
+++ routes/employee.ts
@@ -1,7 +1,30 @@
 // const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
-router.get('/create', (req, res) =>{
+router.get('/all', (req: any, res: {
+    render: any; send: (arg0: string[]) => void; 
+})=>{
+
+    try {
+       var data = fs.readFileSync('./employees.txt', 'utf8');
+       data = data.split('\n')
+       console.log(data);
+    } catch (err) {
+       console.error(err);
+    }
+
+    let main:string[] = data.map(function (entry:string){
+        return entry.split(',').slice(0,3)
+    })
+    
+    res.render('index', {title:"The Express App", message:main});
+})
+
+
+
+router.post('/create', (req: any, res: any) =>{
 
     
 })
+
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 const fs = require('fs')
 
 try {
-   const data = fs.readFileSync('./employees.txt', 'utf8');
+   var data = fs.readFileSync('./employees.txt', 'utf8');
    data = data.split('\n')
    console.log(data);
 } catch (err) {
    console.error(err);
 }
 
+
+let main = data.map(function (entry){
+    return entry.split(',').slice(0,3)
+})
+
+console.log(main)
