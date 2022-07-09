940ae040869a650eb9c2bac6f8593d386fecbd0d
Steinium7 <51533598+Steinium7@users.noreply.github.com>
2022-07-09 15:39:27 +0000

    pagination and writing done

--- routes/employee.ts
+++ routes/employee.ts
@@ -21,10 +21,25 @@ router.get('/all', (req: any, res: {
     res.render('index', {title:"The Express App", message:main});
 })
 
-
+router.get('/:id', async (req: any, res:any)=>{
+    try {
+        var data = fs.readFileSync('./employees.txt', 'utf8');
+        data = data.split('\n')
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+ 
+    let main:string[] = data.map(function (entry:string){
+        return entry.split(',')
+    })
+     
+    res.send(main[req.params.id-1])
+})
 
 router.post('/create', (req: any, res: any) =>{
 
+    const data = req.body
     
 })
 
--- test.js
+++ test.js
@@ -10,7 +10,32 @@ try {
 
 
 let main = data.map(function (entry){
-    return entry.split(',').slice(0,3)
+    return entry.split(',')
 })
 
-console.log(main)
+let arr = main.sort((a,b)=>{return a[1]-b[1]})
+
+
+
+// pagination
+// let page = 2
+// console.log(main.slice(page*10 -10,page*10))
+
+// select single one
+// console.log(main[3-1])
+
+// const some = {
+//    name: 'here',
+//    car: 'some'
+// }
+
+// read and write
+
+// console.log(JSON.stringify(Object.values(some)))
+// let name = JSON.stringify(Object.values(some))
+// let so = name.slice(1,name.length-1)+'\n'
+
+// fs.appendFile("./employees.txt", so, (err) => {
+//    if (err) console.log(err);
+//    console.log("Successfully Written to File.");
+//  });
